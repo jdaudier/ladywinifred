e671a19bfcc454d6f5a7f4489b04963275a18c99
Joanne Daudier <jdaudier@outlook.com>
2015-08-03 01:14:42 -0400

    FEAT: add back-to-top btn

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,44 @@
 $(function() {
+	'use strict';
+
+	// Animated dividers
 	$('.animated-divider').velocity({
 		width: '100%',
 		borderColor: '#f89100'
 	}, {
 		duration: 3000
+	});
+
+	// Smooth scrolling w/ Velocity
+
+	// bind click event to all internal page anchors
+	$('a[href*=#]').bind('click', function(e) {
+
+		// prevent default action and bubbling
+		e.preventDefault();
+		e.stopPropagation();
+
+		// set target to anchor's 'href' attribute
+		var target = $(this).attr('href');
+
+		// scroll to each target
+		$(target).velocity('scroll', {
+			duration: 1000,
+			offset: 20,
+			easing: 'ease-in-out'
+		});
+	});
+
+	// Back to top button
+	var offset = 300; //"back to top" button is shown
+	var $backToTopBtn = $('.top');
+
+	// Hide or show the "back to top" link
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > offset) {
+			$backToTopBtn.addClass('is-visible');
+		} else {
+			$backToTopBtn.removeClass('is-visible');
+		}
 	});
 });
